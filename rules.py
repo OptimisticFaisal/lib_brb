1e0f9a92e008938fc74e2ca57571d47c66b67b72
SonY <faisalcsecubd@gmail.com>
2018-02-03 11:53:40 -0800

    New Variable individual_matching_degree is added

--- rules.py
+++ rules.py
@@ -1,16 +1,11 @@
 class Rules(object):
     def __init__(self):
         self.rule_weight = 1
-        # self.antecedent_ref_val = []
-        # self.antecedent_ref_title = []
-        # self.antecedent_2 = ""
-        # self.antecedent_2_ref_title = ""
         self.parent = ""
         self.combinations = []
+        self.antecedents_belief_dist = []
         self.consequence_val = []
+        self.consequenent_belief_dist = []
         self.matching_degree = None
         self.activation_weight = None
-
-    # def sum_of_activation_weight(self):
-    #     return sum(self.rule_weight * self.matching_degree)
-
+        self.individual_matching = None
